875fcb061375dd0749195a373e24add05310d0f1
saadskamran-del <saadskamran@gmail.com>
2025-08-21 21:27:50 +0500

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -25,13 +25,16 @@ export class App {
     // code that needs to be executes
     console.log('Number clicked: ', numberValue);
     // check if operator is already set
+
     if (this.operator) {
       // if operator is set, then it means we are entering the second number
+      // if value is already available, multiply it with 10 and add the new number
       this.secondNumber = numberValue;
       this.history =
         this.firstNumber?.toString() + ' ' + this.operator + ' ' + this.secondNumber?.toString();
     } else {
       // if operator is not set, then we are entering the first number
+      // if value is already available, multiply it with 10 and add the new number
       this.firstNumber = numberValue;
       this.history = this.firstNumber?.toString();
     }
@@ -49,7 +52,34 @@ export class App {
     if (this.firstNumber && this.secondNumber && this.operator) {
       if (this.operator === '+') {
         this.result = this.firstNumber + this.secondNumber;
+      } else if (this.operator === '-') {
+        this.result = this.firstNumber - this.secondNumber;
+      } else if (this.operator === '*') {
+        this.result = this.firstNumber * this.secondNumber;
+      } else if (this.operator === '/') {
+        this.result = this.firstNumber / this.secondNumber;
+      } else if (this.operator === '%') {
+        this.result = this.firstNumber % this.secondNumber;
       }
+    }
+  }
+  clear() {
+    this.firstNumber = undefined;
+    this.secondNumber = undefined;
+    this.result = undefined;
+    this.operator = undefined;
+    this.history = undefined;
+  }
+  backspace() {
+    // if secondNumber, clear it
+    // else if operator, clear it
+    // else if firstNumber, clear it
+    if (this.secondNumber) {
+      this.secondNumber = undefined;
+    } else if (this.operator) {
+      this.operator = undefined;
+    } else if (this.firstNumber) {
+      this.firstNumber = undefined;
     }
   }
 }
